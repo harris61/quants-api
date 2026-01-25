25a912b06654520988384823eedded9fe6d4940c
harris61 <harristioadam2@gmail.com>
2026-01-25 16:01:19 +0700

    Use venv python in backfill task

--- scripts/foreign_backfill_task.bat
+++ scripts/foreign_backfill_task.bat
@@ -1,3 +1,3 @@
 @echo off
 cd /d C:\Users\User\quants-api
-python main.py collect-foreign --fill-missing --start 2025-01-01 --end 2026-01-01
+C:\Users\User\quants-api\.venv\Scripts\python.exe main.py collect-foreign --fill-missing --start 2025-01-01 --end 2026-01-01
